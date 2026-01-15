2b4fe95765d44eee3f79af9ce7aa731292761adb
West <hu.wenjun1@olaola.chat>
2026-01-15 12:15:54 +0800

    日志开关

--- run_server.py
+++ run_server.py
@@ -5,10 +5,10 @@ from entry import entry
 from loguru import logger as main_logger
 
 if __name__ == '__main__':
-    if os.environ.get("LOGURU_WRITEFILE", "1") == "1":
+    if os.environ.get("LOGURU_WRITEFILE", "1") == "0":
         main_logger.remove()
         # 2. 重新添加：仅绑定标准输出和标准错误
         # 通常建议：INFO 及以下去 stdout，WARNING 及以上去 stderr
-        main_logger.add(sys.stdout, level="INFO", filter=lambda record: record["level"].no < 30)
+        main_logger.add(sys.stdout, level="DEBUG")
         main_logger.add(sys.stderr, level="WARNING")
     entry.main()
