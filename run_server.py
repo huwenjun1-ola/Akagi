0ef14bec00bae8167fd97c1bea4478cddf225e3e
West <hu.wenjun1@olaola.chat>
2026-01-15 12:05:54 +0800

    日志开关

--- run_server.py
+++ run_server.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
         main_logger.remove()
         # 2. 重新添加：仅绑定标准输出和标准错误
         # 通常建议：INFO 及以下去 stdout，WARNING 及以上去 stderr
-        main_logger.add(sys.stdout, level="DEBUG")
-        main_logger.add(sys.stderr, level="DEBUG")
+        main_logger.add(sys.stdout, level="INFO", filter=lambda record: record["level"].no < 30)
+        main_logger.add(sys.stderr, level="WARNING")
     entry.main()
